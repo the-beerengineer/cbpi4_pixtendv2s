98d8bbaa53abb225d0f6b6e11e52e4959b5647fc
the-beerengineer <accounts+github@hschlaak.de>
2022-04-07 21:21:06 +0200

    added info to setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(name='cbpi4_pixtendv2s',
       version='0.0.1',
-      description='CraftBeerPi Plugin',
-      author='',
-      author_email='',
-      url='',
+      description='CraftBeerPi4 Plugin for PiXtendV2S',
+      author='the-beerengineer',
+      author_email='accounts+github@hschlaak.de',
+      url='https://github.com/the-beerengineer/cbpi4_pixtendv2s',
       include_package_data=True,
       package_data={
         # If any package contains *.txt or *.rst files, include them:
       '': ['*.txt', '*.rst', '*.yaml'],
       'cbpi4_pixtendv2s': ['*','*.txt', '*.rst', '*.yaml']},
-      packages=['cbpi4_pixtendv2s'],
+      packages=['cbpi4_pixtendv2s', 'pplv2'],
      )
